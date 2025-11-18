081aa52a9ffbc3f45f4aa668a7c133ded12c2011
aalinasever <severskayaalina@gmail.com>
2025-11-18 23:32:28 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,4 +2,21 @@
 select count(*) as customers_count 
 from customers cust
 
---
+-- отчет с продавцами у которых наибольшая выручка
+select 
+	concat(first_name, ' ', last_name) as seller
+	,
+	count(sale.sales_person_id) as operations
+	,
+	floor(SUM(sale.quantity * prod.price)) as income
+from
+	employees emp
+inner join sales sale on
+	emp.employee_id = sale.sales_person_id
+inner join products prod on
+	prod.product_id = sale.product_id
+group by
+	seller
+order by
+	income desc
+limit 10
